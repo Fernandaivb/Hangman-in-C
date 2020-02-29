e6302c1851c112ca5daf076db382a0d879847330
crockerk1 <61515688+crockerk1@users.noreply.github.com>
2020-02-29 15:14:50 -0700

    Add files via upload

--- startGame.c
+++ startGame.c
@@ -8,36 +8,51 @@
 #include <time.h>
 #include <stdlib.h>
 
-int startGame(){
-    int yes=1, no=2, userIn=0, playerCount=0;
+char custom[100];
+char customWord(){
     char custom[100];
-    printf("Welcome to Hangman! Would you like to play? (Yes=1 and No=2)");
-    scanf("%d", &userIn);
-    if (userIn==1){
+    printf("Player 1 enter a word: ");
+    scanf("%s", custom);
+    return custom;
+}
+//int randomWord(){
+    //movies= fopen(bigproject.txt, "r"); 
+    //random word generator
+    //srand(time(NULL)); seed generator
+//}
+int startGame(){
+    char userIn[5]; 
+    int playerCount=0;
+    printf("Welcome to Hangman! Would you like to play? (Yes=y and No=n)");
+    scanf("%s", userIn);
+    if (strcmp (userIn,"y")==0){
         printf("Would you like to play with 1 player or 2 players?");
         scanf("%d", &playerCount);
         if (playerCount==1){
-            printf("You are now playing with 1 player!\n");
+            printf("You are now playing with 1 player!\n ");
+            //randomWord();
         }
         else if (playerCount==2){
             printf("You are now playing with 2 players!\n");
+            customWord();
         }
         while ((playerCount!=1) && (playerCount!=2)){
             printf("Invalid answer. Would you like to play 1 player or 2 players?");
             scanf("%d", &playerCount);
         }
     }
-    else if (userIn==2){
-        printf("You are not playing.\n");
+    else if (strcmp(userIn, "n")==0){
+        printf("You are not playing.");
     }
-    while ((userIn != 1) &&  (userIn !=2)){
+    while ((strcmp (userIn, "y")!= 0) && (strcmp(userIn, "n")!=0)){
         printf("That is an invalid answer. Would you like to play hangman? (Yes=1 and No=2)");
-        scanf("%d", &userIn);
+        scanf("%s", userIn);
     }
-}
-  
+} 
 int main (){
     startGame();
-    
+    printf("%c", custom); 
+
+    //fclose();
     return 0;
 }
